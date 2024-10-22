735ec20523283f496022f5dc0f8c3e1a7105e78b
Brown Kudi <60327383+brownkudicodes@users.noreply.github.com>
2024-10-22 10:33:18 +0200

    simple game with 5 guesses

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <cstdlib>
+#include <random>
 
 // number guessing game (with 5 guesses)
+int getRandomNumber() {
+    srand(static_cast<unsigned>(time(nullptr)));
+    const int random = 1+ (rand() % 100);
+    return random;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const int random_number = getRandomNumber();
+
+    bool guessed = false;
+
+    for (int i = 1; i <= 5; i++) {
+        int n;
+        std::cin >> n;
+        if (random_number == n) {
+            std::cout << "Good Job!!" << '\n';
+            return 1;
+        }
+        std::cout << n << " was not correct." << '\n';
+    }
+
+    std::cout << "You lost, it was " << random_number << std::endl;
     return 0;
 }
